6de903ca0f65fa237238012a96061a6c0deb4507
EricSchles <Ericschles@gmail.com>
2015-04-29 15:00:48 -0400

    Update redblacktree.cpp

--- cpp/redblacktree.cpp
+++ cpp/redblacktree.cpp
@@ -2,12 +2,15 @@
 using namespace std;
 
 //make use of this: http://en.wikipedia.org/wiki/Red%E2%80%93black_tree
+//and this: http://www.sanfoundry.com/cpp-program-implement-red-black-tree/
 class Node{
 public:
 	int data;
 	string color;
 	Node* left;
 	Node* right;
+	Node* parent;
+	int key;
 	Node(int datum, string coloring,Node* lefty,Node* righty){
 		data = datum;
 		color = coloring;
@@ -22,14 +25,49 @@ class RedBlackTree{
 public:
 	Node* root;
 	RedBlackTree(){}
+	bool exists(int data){
+		if(root!=NULL){
+			if(root->data==data){
+				return true;
+			}else{
+				return exists(data,root);
+			}
+		}else{
+			return false;
+		}
+	}
+
 	void insert(int data){
-		if(root=NULL){
-			root = new Node(data,"black",NULL,NULL);
+		if(root==NULL){
+			root = new Node(data,"red",NULL,NULL);
+		}else{
+
+			insert(data,root);
 		}
 	}
 	void remove(int data);
+	
 private:
-	void insert(int data, Node* leaf);
+	bool exists(int data,Node* cur){
+		if(cur==NULL){
+			return false;
+		}
+		if(cur-> data == data){
+			return true;
+		}
+		if(cur->data < data){
+			exists(data,cur->left);
+		}else{
+			exists(data,cur->right);
+		}
+	}
+
+	void insert(int data, Node* leaf){
+
+	}
+	void rotate_left(Node* ){
+
+	}
 	void remove(int data, Node* leaf);
 };
 
